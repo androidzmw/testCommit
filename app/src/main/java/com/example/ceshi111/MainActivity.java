073c7fb09877e4913ae831bddf6c2a01cc19678e
androidzmw <m18810135220@163.com>
2023-10-17 09:40:44 +0800

    更新 MainActivity.java

--- app/src/main/java/com/example/ceshi111/MainActivity.java
+++ app/src/main/java/com/example/ceshi111/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("123", "onCreate:45 ");
+        Log.d("123", "onCreate:45555 ");
     }
 }
